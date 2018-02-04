2b68e871ada1bf00721864dd172080d4b1583151
Shriya Nevatia <sneva831@gmail.com>
2018-02-04 09:39:28 -0800

    working routing linking page to page

--- teho_package/routes.py
+++ teho_package/routes.py
@@ -7,6 +7,26 @@ from teho_package import teho
 def index():
     return render_template('index.html')
 
-@teho.route('/onboard')
-def onboard():
-	return render_template('onboard.html')
+@teho.route('/onboard_1')
+def onboard_1():
+	return render_template('onboard-1.html')
+
+@teho.route('/onboard_2')
+def onboard_2():
+	return render_template('onboard-2.html')
+
+@teho.route('/onboard_3')
+def onboard_3():
+	return render_template('onboard-3.html')
+
+@teho.route('/log_before')
+def log_before():
+	return render_template('log-before.html')
+
+@teho.route('/breathe')
+def breathe():
+	return render_template('breathe.html')
+
+@teho.route('/log_after')
+def log_after():
+	return render_template('log-after.html')
