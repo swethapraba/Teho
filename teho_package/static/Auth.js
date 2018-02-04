4b8ca0e2d1183a05fd1f1aeca216c33526ec3d94
carolw21 <carolw2000@gmail.com>
2018-02-03 23:43:46 -0800

    FIXED EveryTHING

--- teho_package/static/Auth.js
+++ teho_package/static/Auth.js
@@ -122,7 +122,6 @@ function loadDayEvents() {
     if (events.length > 0) {
       for (i = 0; i < events.length; i++) {
         var event = events[i];
-        console.log(event.start.dateTime.getDay());
         var start = event.start.dateTime.toString();
         var end = event.end.dateTime.toString();
         var parsedStart = parseStartTime(start);
@@ -163,7 +162,8 @@ function callEventAdd()
   //var start = "2018-05-28T17:00:00-07:00";
   //var end = "2018-05-28T09:00:00-07:00";
   //addEventToCal('meditate',5,'www.google.com', start, end,'America/LosAngeles');
-  addEventToCal('meditate',5,'www.google.com');
+  //addEventToCal('meditate',5,'www.google.com');
+  addEventToCal();
 }
 
 /**
@@ -171,32 +171,33 @@ function callEventAdd()
  * Outcome: add event to GCal with provided specs
  */
 //function addEventToCal(activity,duration,url,starting,ending,tzone)
-function addEventToCal(activity,duration,url)
+//function addEventToCal(activity,duration,url)
+function addEventToCal()
 {
   //starting = '2018-02-28T09:00:00-07:00';
   //ending = '2018-02-28T17:00:00-07:00';
 
-  var eventSummary = "Take " + duration + "minutes to " + activity; //'summary' field needs a string.PARAM
-  var buildDescript = "Here\'s Teho\'s recommendation for you: " + url; //build string
+  //var eventSummary = "Take " + duration + "minutes to " + activity; //'summary' field needs a string.PARAM
+  //var buildDescript = "Here\'s Teho\'s recommendation for you: " + url; //build string
   var startDate = '2018-05-28T17:00:00-07:00';//starting;
-  var endDate = '2018-05-28T09:00:00-07:00'//ending;
-  var zone = 'America/LosAngeles'//tzone;
+  var endDate = '2018-05-28T09:00:00-07:00';//ending;
+  var zone = 'America/LosAngeles';//tzone;
   var event = {
-    'summary': eventSummary,//
-    'description': buildDescript,//
+    'summary': 'test',//eventSummary,//
+    'description': 'test',//buildDescript,//
     'colorId': '1',//This or code 3 is the purple shade we want, will have to experiment
     'start':
     {
-      'dateTime': startDate,//
-      'timeZone': zone
+      'dateTime': '2018-05-28T09:00:00-07:00',//startDate,//
+      'timeZone': 'America/Los_Angeles'//zone
     },
     'end': //
     {
-      'dateTime': endDate,//
-      'timeZone': zone
+      'dateTime': '2018-05-28T17:00:00-07:00',//endDate,//
+      'timeZone': 'America/Los_Angeles'//zone
     },
     /**'attendees': [//// we might not need this since just insert to primary calendar. made for sharing
-      {'email': 'lpage@example.com'},////
+      {'email': 'lpageexample.com'},////
     ],**/
     'reminders': {//double check fyi
       'useDefault': false,// double check that this stops all reminders
